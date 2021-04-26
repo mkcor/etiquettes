012e35a7af4a7370cec7037ef6da954ed2e91819
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2021-04-26 20:14:17 +0200

    Modularize address lookup

--- imprime_etiquettes.py
+++ imprime_etiquettes.py
@@ -4,27 +4,31 @@ import numpy as np
 import pandas as pd
 
 
+def find_address_France(adresse):
+    # Find postal code in address
+    cp_p = re.compile('(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}')
+    search = cp_p.search(adresse)
+    if search is None:
+        return ''
+    else:
+        ad1 = adresse[:search.span()[0]].replace(',','')
+        ad2 = adresse[search.span()[0]:].replace('.','').upper()
+        return [ad1, ad2]
+
+
 def print_one_order(i):
     # Load data for row i
     prenom = df.loc[i, 'Prénom']
     nom = df.loc[i, 'Nom']
     adresse = df.loc[i, 'Adresse']
+    ad = find_address_France(adresse)
+    ad_lines = [f'<h1>{a}</h1>' for a in ad]
     commande = df.loc[i, 'Commande']
-    # Find postal code in address
-    cp_p = re.compile('(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}')
-    search = cp_p.search(adresse)
-    if search is None:
-        ad1 = ''
-        ad2 = ''
-    else:
-        ad1 = adresse[:search.span()[0]].replace(',','')
-        ad2 = adresse[search.span()[0]:].replace('.','')
-
     items = [f'<li>{c}</li>' for c in commande.split(';')]
-    string = (
-        f'<h1 class="tiny-margin"> {prenom} {nom.upper()} </h1>'
-        f'<h1> {ad1} </h1>'
-        f'<h1> {ad2.upper()} </h1>'
+    string = f'<h1 class="tiny-margin"> {prenom} {nom.upper()} </h1>'
+    for al in ad_lines:
+        string += al
+    string += (
         '<p class="top-margin">Expéditeur :</p>'
         '<p>Lorem ipsum dolor sit amet</p>'
         '<p>67000 CONSECTETUR</p>'
